05a21c71d7a65c0295e4d00401c892e865d6472d
inocop <inoue19851110@gmail.com>
2018-08-24 12:24:00 +0900

    デプロイ処理を修正

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,10 +5,15 @@ set -eux
 source ./config
 
 git clone --depth 1 --single-branch -b $BRANCH $REPOSITORY rails_app
-rm -rf rails_app/.git
-tar -zcvf rails_app.tar.gz rails_app
+# .gitとdeployフォルダは不要なので削除
+rm -rf rails_app/.git deploy
 
+# tar.gzに圧縮して転送
+tar -zcvf rails_app.tar.gz rails_app
 scp -i ${SECRET_KEY} rails_app.tar.gz ${REMOTE_USER}@${REMOTE_SERVER}:/tmp/
+
+# デプロイタスクのshellをsshで流し込み
 ssh -i ${SECRET_KEY} ${REMOTE_USER}@${REMOTE_SERVER} bash -s < update_app.sh
 
+# 後処理のファイル削除
 rm -f rails_app.tar.gz
--- deploy/update_app.sh
+++ deploy/update_app.sh
@@ -2,12 +2,14 @@ set -eux
 
 sudo su
 
+# 転送した圧縮ファイルを展開
 cd /home/railsdev
 if [ -e rails_app ]; then 
   mv rails_app rails_app_$(date +%Y%m%d_%H%M%S) 
 fi
 tar -zxvf /tmp/rails_app.tar.gz
 
+# デプロイタスクを実行
 cd /home/railsdev/rails_app/docker/rails_prd
 #docker-compose build
 docker exec rails_prd_1 bash -c    'cd public && npm install'
@@ -15,3 +17,6 @@ docker exec rails_prd_1 bash -l -c 'bundle install'
 docker exec rails_prd_1 bash -l -c 'bundle exec rake db:migrate RAILS_ENV=production'
 #docker exec rails_prd_1 bash -l -c 'bundle exec rake assets:precompile'
 docker-compose restart
+
+# 5世代以上前を削除
+ls -lt | tail -n +5 | xargs rm -rf
