b53102d99cb631215ee99375439b03db4c2403be
inocop <inoue19851110@gmail.com>
2018-08-24 17:28:55 +0900

    更新処理を修正

--- deploy/update_app.sh
+++ deploy/update_app.sh
@@ -1,10 +1,10 @@
 set -eux
 
 sudo su
+cd /home/railsdev
 
 
 # 転送した圧縮ファイルを展開
-cd /home/railsdev
 if [ -e rails_app ]; then 
   mv rails_app rails_app_$(date +%Y%m%d_%H%M%S)
 fi
@@ -18,11 +18,11 @@ docker-compose restart web
 
 
 # db、パッケージマネージャを更新
-docker exec rails_prd_web_1 bash -l -c 'cd public && npm install'
+docker exec rails_prd_web_1 bash -c    'cd public && npm install'
 docker exec rails_prd_web_1 bash -l -c 'bundle install'
 docker exec rails_prd_web_1 bash -l -c 'bundle exec rake db:migrate RAILS_ENV=production'
 #docker exec rails_prd_web_1 bash -l -c 'bundle exec rake assets:precompile'
 
 
 # 5世代分残して削除
-ls -t | grep rails_app | tail -n+6 | xargs rm -rf
+ls -t | grep rails_app_ | tail -n+6 | xargs rm -rf
