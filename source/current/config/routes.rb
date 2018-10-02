2bf7b78f1f7f5d3e2188fa1a455dce51a929e794
inocop <inoue19851110@gmail.com>
2018-10-02 12:43:51 +0900

    deviseの設定変更

--- source/current/config/routes.rb
+++ source/current/config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  # ログイン・ログアウト以外のdeviseルーティングを制限
-  devise_for :users, skip: :all
-  devise_scope :user do
-    get    'users/sign_in'  => 'devise/sessions#new',     as: :new_user_session
-    post   'users/sign_in'  => 'devise/sessions#create',  as: :user_session
-    delete 'users/sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
-  end
+  devise_for :users
+
+  # === deviseのルーティング制限
+  # devise_for :users, skip: :all
+  # devise_scope :user do
+  #   get    'users/sign_in'  => 'devise/sessions#new',     as: :new_user_session
+  #   post   'users/sign_in'  => 'devise/sessions#create',  as: :user_session
+  #   delete 'users/sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
+  # end
 
   root 'dashboard#index'
   get 'dashboard', :to => 'dashboard#index'
