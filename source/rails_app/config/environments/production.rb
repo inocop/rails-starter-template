61205a724e86d5745c9a1f1fdc968ad9a949b004
inocop <inoue19851110@gmail.com>
2018-11-06 17:13:27 +0900

    デバッグでpumaを使うとjsやcssがロードできなくなるので、sendfile設定をproducetionのみ適用

--- source/rails_app/config/environments/production.rb
+++ source/rails_app/config/environments/production.rb
@@ -28,8 +28,12 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  #####################
+  # change config     #
+  #####################
+  # sendfileでの送信をwebサーバで処理する
+  config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
