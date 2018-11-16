82ec2a319b1ac99b9d6458499d362bb724bed248
inocop <inoue19851110@gmail.com>
2018-11-16 22:07:06 +0900

    setup.shのコメントをGemfileへ移動

--- app/setup.sh
+++ app/setup.sh
@@ -8,41 +8,21 @@ fi
 RAILS_APP_DIR=/var/my_dir/app/rails_app
 NODE_APP_DIR=/var/my_dir/app/node_app
 
-
 set -eux
 cd ${RAILS_APP_DIR}
 
-
-###########################
-#  create project         #
-###########################
-# gem install rails -v '5.1.6'
-# rails _5.1.6_ new . --database=mysql --skip-bundle --skip-coffee --skip-turbolinks --skip-sprockets
-
-###########################
-#  change file            #
-###########################
-# edit config/database.yml
-#        use myconf.yml
-#
-# edit config/secrets.yml
-#        use myconf.yml
-#
-# edit config/application.rb
-#        use myconf.yml
-
-
-# package install
-bundle config --local build.nokogiri --use-system-libraries
-bundle install --path vendor/bundle
-
-npm install --prefix ${RAILS_APP_DIR}/public
-sudo npm install --prefix ${NODE_APP_DIR}
-
+# package install(railsdevユーザでnpm install)
+su -s /bin/bash railsdev -c "bundle config --local build.nokogiri --use-system-libraries"
+su -s /bin/bash railsdev -c "bundle install --path vendor/bundle"
+su -s /bin/bash railsdev -c "npm install --prefix ${RAILS_APP_DIR}/public"
+su -s /bin/bash railsdev -c "npm install --prefix ${NODE_APP_DIR}"
+if [ "${RAILS_ENV}" = "production" ]; then
+  exit
+fi
 
 # db create
 bundle exec rake db:create          RAILS_ENV=${RAILS_ENV}
-bundle exec rake db:schema:load     RAILS_ENV=${RAILS_ENV} DISABLE_DATABASE_ENVIRONMENT_CHECK=1 # or bundle exec rake db:migrate
+bundle exec rake db:migrate:reset   RAILS_ENV=${RAILS_ENV} DISABLE_DATABASE_ENVIRONMENT_CHECK=1 # or bundle exec rake db:schema:load
 bundle exec rake db:seed            RAILS_ENV=${RAILS_ENV}
 bundle exec rake db:environment:set RAILS_ENV=${RAILS_ENV}
 
