5db2d935f4585d541d38c7d15f2a72613ede9fe0
inocop <inoue19851110@gmail.com>
2018-11-22 21:00:32 +0900

    ダッシュボードのグラフをプロジェクト単位に修正

--- app/rails_app/app/controllers/authed/dashboard_controller.rb
+++ app/rails_app/app/controllers/authed/dashboard_controller.rb
@@ -2,20 +2,18 @@ class Authed::DashboardController < AuthController
   layout 'authed/dashboard'
 
   def index
-    # ユーザー一覧
-    users = User.all
-    # ステータス別にticketを分割
-    tickets = Ticket.where("project_id = ?", current_project_id)
+    projects = Project.active
+    tickets = Ticket.active
 
-    @email_list = []
-    @draft_date_list = []
-    @start_date_list = []
-    @end_date_list   = []
-    users.each do |user|
-      @email_list << user.email
-      @draft_date_list << tickets.select{|item| user.id == item.assigned_user_id && item.status == Ticket::STATUS_DRAFT}.count
-      @start_date_list << tickets.select{|item| user.id == item.assigned_user_id && item.status == Ticket::STATUS_START}.count
-      @end_date_list   << tickets.select{|item| user.id == item.assigned_user_id && item.status == Ticket::STATUS_END}.count
+    @project_list = []
+    @draft_ticket_list = []
+    @start_ticket_list = []
+    @end_ticket_list   = []
+    projects.each do |project|
+      @project_list << project.name
+      @draft_ticket_list << tickets.select{|item| project.id == item.project_id && item.status == Ticket::STATUS_DRAFT}.count
+      @start_ticket_list << tickets.select{|item| project.id == item.project_id && item.status == Ticket::STATUS_START}.count
+      @end_ticket_list   << tickets.select{|item| project.id == item.project_id && item.status == Ticket::STATUS_END}.count
     end
 
   end
