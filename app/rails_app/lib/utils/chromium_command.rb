569fd98548b313e3aebdda7aeb75325bc91bb614
inocop <inoue19851110@gmail.com>
2018-11-29 20:32:21 +0900

    ChromiumCommandにタイムアウト処理を追加

--- app/rails_app/lib/utils/chromium_command.rb
+++ app/rails_app/lib/utils/chromium_command.rb
@@ -1,36 +1,45 @@
 require 'shellwords'
+require 'timeout'
 
-module Utils
-  class ChromiumCommand
+class ChromiumCommand
 
-    def initialize(width: 1366, height: 768)
-      @width = width.to_i
-      @height = height.to_i
-    end
-
-    # screenshot出力
-    def html_to_png(html:, save_path:)
-      system(*[AppConst::CHROMIUM_PATH,
-               "--headless",
-               "--disable-gpu",
-               "--no-sandbox",
-               "--window-size=#{@width},#{@height}",
-               "--screenshot=#{Shellwords.escape(save_path)}",
-               html]
-            )
-    end
-
-    # pdf出力
-    def html_to_pdf(html:, save_path:)
-      system(*[AppConst::CHROMIUM_PATH,
-               "--headless",
-               "--disable-gpu",
-               "--no-sandbox",
-               "--window-size=#{@width},#{@height}",
-               "--print-to-pdf=#{Shellwords.escape(save_path)}",
-               html]
-            )
-    end
-
+  def initialize(width: 1366, height: 768)
+    @width = width.to_i
+    @height = height.to_i
   end
+
+  # screenshot出力
+  def html_to_png(html:, save_path:)
+    begin
+      Timeout.timeout(5) do
+        system(*[AppConst::CHROMIUM_PATH,
+                "--headless",
+                "--disable-gpu",
+                "--no-sandbox",
+                "--window-size=#{@width},#{@height}",
+                "--screenshot=#{Shellwords.escape(save_path)}",
+                html])
+      end
+    rescue Timeout::Error
+      false
+    end
+  end
+
+  # pdf出力
+  def html_to_pdf(html:, save_path:)
+    begin
+      Timeout.timeout(5) do
+        system(*[AppConst::CHROMIUM_PATH,
+                  "--headless",
+                  "--disable-gpu",
+                  "--no-sandbox",
+                  "--window-size=#{@width},#{@height}",
+                  "--print-to-pdf=#{Shellwords.escape(save_path)}",
+                  html])
+      end
+    rescue Timeout::Error
+      false
+    end
+  end
+
 end
