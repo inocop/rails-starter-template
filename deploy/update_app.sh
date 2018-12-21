3db72e6093505eb252a2e2f315af68c7a270b121
inocop <inoue19851110@gmail.com>
2018-12-21 14:10:17 +0900

    デプロイの不具合を修正

--- deploy/update_app.sh
+++ deploy/update_app.sh
@@ -5,17 +5,15 @@ set -eux
 ############################
 #  変数定義                #
 ############################
-SOURCE_DIR_NAME="source"
+RELEASE_ROOT="/release"
 
-# リリースディレクトリ
-RELEASE_DIR="/release/${SOURCE_DIR_NAME}"
-# 共有ディレクトリ
-SHARED_DIR="/release/shared"
-# デプロイ用の一時ディレクトリ
-DEPLOY_DIR="/release/deploy"
-# コンテナ内のRails.root
-DOCKER_RAILS_ROOT="/var/www/app/${SOURCE_DIR_NAME}/rails_app"
+RELEASE_DIR="${RELEASE_ROOT}/source"  # リリースディレクトリ
+SHARED_DIR="${RELEASE_ROOT}/shared"   # 共有ディレクトリ
+DEPLOY_DIR="${RELEASE_ROOT}/deploy"   # デプロイ用ディレクトリ
 
+# Dockerコンテナ内のディレクトリ
+DOCKER_DEPLOY_DIR="/var/www/app/deploy/rails_app"
+DOCKER_RELEASE_DIR="/var/www/app/source/rails_app"
 
 # ホスト、コンテナの両方でシンボリックリンクが効くように相対パス指定
 declare -A LINK_DIRS
@@ -33,9 +31,10 @@ function docker_cmd() {
   if [ "$1" = "exec" ]; then
     WEB_CONTAINER_ID=`sudo docker ps -f name=rails_prd_web_1 -q`
     sudo docker exec $WEB_CONTAINER_ID bash -c "$2"
+
   elif [ "$1" = "build" ]; then
     pushd ${RELEASE_DIR}/docker/rails_prd/
-      sudo docker image prune -f # 不要なimageを削除
+      sudo docker image prune -f  # 不要なimageを削除
       sudo bash -c "MY_DOMAIN=${MY_DOMAIN} docker-compose up -d --build"
     popd
   else
@@ -44,17 +43,15 @@ function docker_cmd() {
 }
 
 ############################
-#  前処理                   #
+#  前処理                  #
 ############################
-# 前回のデプロイデータがあれば削除
-sudo rm -rf ${DEPLOY_DIR}
-
 # ディレクトリ作成
 sudo mkdir -p ${RELEASE_DIR}
 sudo mkdir -p ${SHARED_DIR}
 sudo mkdir -p ${DEPLOY_DIR}
 
 # source.tar.gzをデプロイ用ディレクトリに展開
+sudo rm -rf ${DEPLOY_DIR}/*
 sudo tar -zxf /tmp/source.tar.gz -C ${DEPLOY_DIR} --strip-components 1
 
 # sharedディレクトリにシンボリックリンクを設定
@@ -72,12 +69,12 @@ sudo chown -R 1000:1000 ${SHARED_DIR}
 #  初回デプロイ             #
 ############################
 if [ "$OPTION" = "init" ]; then
-  sudo chown -R 1000:1000 ${RELEASE_DIR}/../
-  rm -rf ${RELEASE_DIR}
-  mv ${DEPLOY_DIR} ${RELEASE_DIR}
+  sudo chown -R 1000:1000 ${RELEASE_ROOT}
+  sudo rm -rf           ${RELEASE_DIR}
+  sudo mv ${DEPLOY_DIR} ${RELEASE_DIR}
 
   docker_cmd "build"
-  docker_cmd "exec" "bash ${DOCKER_RAILS_ROOT}/../setup.sh production"
+  docker_cmd "exec" "bash /var/www/app/source/setup.sh production"
   echo "Successfully init deployed"
   exit # 初回デプロイはここで終了
 fi
@@ -87,30 +84,27 @@ fi
 #  コンテナ内処理           #
 ############################
 # gem、npmの更新
-docker_cmd "exec" "cd ${DOCKER_RAILS_ROOT} \
+docker_cmd "exec" "cd ${DOCKER_DEPLOY_DIR} \
                    && su -s /bin/bash railsdev -c \"npm install --prefix ./lib/nodejs\" \
                    && su -s /bin/bash railsdev -c \"npm install --prefix ./public\" \
                    && su -s /bin/bash railsdev -c \"bundle install\""
-
 # テスト実行
-docker_cmd "exec" "cd ${DOCKER_RAILS_ROOT} \
+docker_cmd "exec" "cd ${DOCKER_DEPLOY_DIR} \
                    && RAILS_ENV=test  bin/rails db:migrate"
                     #&& bin/rails test:system test
-
 #  DBマイグレーション
-docker_cmd "exec" "cd ${DOCKER_RAILS_ROOT} \
+docker_cmd "exec" "cd ${DOCKER_DEPLOY_DIR} \
                    && RAILS_ENV=production  bin/rails db:migrate"
-
 # cron設定
-docker_cmd "exec" "cd ${DOCKER_RAILS_ROOT} \
+docker_cmd "exec" "cd ${DOCKER_DEPLOY_DIR} \
                    &&  RAILS_ENV=production  bundle exec whenever --update-crontab"
 
 
 ############################
 #  source切り替え           #
 ############################
-mv ${RELEASE_DIR} ${RELEASE_DIR}_$(date +%Y%m%d_%H%M%S)
-mv ${DEPLOY_DIR}  ${RELEASE_DIR}
+sudo mv ${RELEASE_DIR} ${RELEASE_DIR}_$(date +%Y%m%d_%H%M%S)
+sudo mv ${DEPLOY_DIR}  ${RELEASE_DIR}
 
 
 ############################
@@ -119,7 +113,7 @@ mv ${DEPLOY_DIR}  ${RELEASE_DIR}
 if [ "$OPTION" = "build" ]; then
   docker_cmd "build"
 else
-  docker_cmd "exec" "passenger-config restart-app ${DOCKER_RAILS_ROOT} \
+  docker_cmd "exec" "passenger-config restart-app ${DOCKER_RELEASE_DIR} \
                      && systemctl reload delayed_job"
 fi
 
@@ -127,9 +121,8 @@ fi
 #############################
 #  sourceを5世代分残して削除  #
 #############################
-pushd ${RELEASE_DIR}/../
-  ls | grep ${SOURCE_DIR_NAME}_ | head -n -5 | xargs rm -rf
+pushd ${RELEASE_ROOT}
+  ls | grep source_ | head -n -5 | xargs sudo rm -rf
 popd
-
 
 echo "Successfully deployed"
